fe2f39e34eeeb8a89a8bba5cc2336fbcf7f5057b
maianh041103 <nguyenmaianh041103@gmail.com>
2023-11-28 02:30:57 +0700

    Caì đặt CORS

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import dotenv from "dotenv";
 import * as database from "./config/database"
@@ -21,6 +22,15 @@ database.connect();
 
 //Nhúng route
 routeAdmin(app);
+
+//Nhúng cors
+// var corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.listen(port, (): void => {
   console.log("App listen on " + port);
