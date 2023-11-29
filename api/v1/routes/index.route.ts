1c807629816f61d5dcbb008e8e932fd1236f44e2
maianh041103 <nguyenmaianh041103@gmail.com>
2023-11-30 00:08:06 +0700

    Tạo route private

--- api/v1/routes/index.route.ts
+++ api/v1/routes/index.route.ts
@@ -1,10 +1,11 @@
 import { Express } from "express";
 import { taskRouter } from "./task.route";
 import { userRoute } from "./user.route";
+import * as authMiddleware from "../middlerware/auth.middlerware";
 
 const routeAdmin = (app: Express): void => {
   const version: string = "/api/v1";
-  app.use(version + '/tasks', taskRouter);
+  app.use(version + '/tasks', authMiddleware.auth, taskRouter);
   app.use(version + '/users', userRoute);
 }
 
